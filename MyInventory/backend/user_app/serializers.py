1dfecc30f21cec812e7edf091594f15eddbabbb1
pasani01 <73846361+pasani01@users.noreply.github.com>
2026-02-21 13:01:06 +0500

    d

--- MyInventory/backend/user_app/serializers.py
+++ MyInventory/backend/user_app/serializers.py
@@ -13,13 +13,22 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'role', 'company']
 
 
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-    
+
     def validate(self, data):
-        user = authenticate(username=data['username'], password=data['password'])
-        if user and user.is_active:
-            data['user'] = user
-            return data
-        raise serializers.ValidationError("Yanlış kullanıcı adı veya şifre")
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+        if not user:
+            raise serializers.ValidationError("Geçersiz kullanıcı adı veya şifre")
+        if not user.is_active:
+            raise serializers.ValidationError("Kullanıcı pasif")
+
+        data['user'] = user
+        return data
