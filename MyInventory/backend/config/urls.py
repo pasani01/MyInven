3a47d535bc4468fb500c003964c88593db92f4f5
pasani01 <73846361+pasani01@users.noreply.github.com>
2026-02-23 15:39:13 +0500

    wq

--- MyInventory/backend/config/urls.py
+++ MyInventory/backend/config/urls.py
@@ -29,6 +29,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("healthz/", lambda request: HttpResponse("OK")),
     path('admin/logout/', admin.site.logout, name='admin-logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('app.urls')),
@@ -40,7 +41,4 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-]
-urlpatterns += [
-    path("healthz/", lambda request: HttpResponse("OK")),
 ]
