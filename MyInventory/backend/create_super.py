4b82e5f41150d238b8520cd232919d8cfcec9584
pasani01 <73846361+pasani01@users.noreply.github.com>
2026-02-24 17:51:00 +0500

    dw

--- MyInventory/backend/create_super.py
+++ MyInventory/backend/create_super.py
@@ -1,3 +1,4 @@
+
 import os
 import django
 
@@ -11,14 +12,14 @@ username = os.environ.get('SUPER_USERNAME', 'admin')
 email    = os.environ.get('SUPER_EMAIL', 'admin@admin.com')
 password = os.environ.get('SUPER_PASSWORD', 'Admin1234!')
 
-if User.objects.filter(username=username).exists():
-    u = User.objects.get(username=username)
-    u.role = 'superadmin'  # ← save() otomatik is_staff ve is_superuser=True yapar
+if User.objects.filter(email=email).exists():
+    u = User.objects.get(email=email)
+    u.role = 'superadmin'
     u.set_password(password)
     u.save()
-    print(f"User '{username}' updated to superadmin.")
+    print(f"User with email '{email}' updated to superadmin.")
 else:
     u = User(username=username, email=email, role='superadmin')
     u.set_password(password)
     u.save()
-    print(f"Superuser '{username}' created successfully!")
+    print(f"Superuser with email '{email}' created successfully!")
