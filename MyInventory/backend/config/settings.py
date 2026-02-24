c8c40f369ee525ce36b4903cf5c9f6e0c739f6ab
pasani01 <73846361+pasani01@users.noreply.github.com>
2026-02-24 09:23:01 +0500

    dawww

--- MyInventory/backend/config/settings.py
+++ MyInventory/backend/config/settings.py
@@ -72,13 +72,19 @@ REST_FRAMEWORK = {
 }
 
 # ─── DATABASE ─────────────────────────────────────────────────────────────────
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ['DATABASE_URL'],
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ─── AUTH ─────────────────────────────────────────────────────────────────────
 AUTH_USER_MODEL = 'user_app.CustomUser'
