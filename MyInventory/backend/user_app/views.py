5fe5319ce6733ff2b6e59d3e6de106e5d13b38a9
pasani01 <73846361+pasani01@users.noreply.github.com>
2026-02-25 15:17:10 +0500

    hadi

--- MyInventory/backend/user_app/views.py
+++ MyInventory/backend/user_app/views.py
@@ -16,7 +16,7 @@ class CompanyViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
 
-class IsAdminRole(permissions.BasePermission):
+class IsAdminRole(permissions.BasePermission): 
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
             return False
